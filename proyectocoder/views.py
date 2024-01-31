3cacb883598b6f8d972ab385c5e746063dca2327
Mateo DM <104037425+mateodm@users.noreply.github.com>
2024-01-31 15:54:57 -0300

    CRUD

--- proyectocoder/views.py
+++ proyectocoder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse
 from proyectocoder.models import *
@@ -29,6 +29,28 @@ def post_gorras(request):
         form = GorraForm()
     return render(request, 'gorras.html', {'form': form})
 
+def editar_gorra(request, id):
+    gorra = get_object_or_404(Gorras, id=id)
+
+    if request.method == 'POST':
+        form = GorraForm(request.POST, instance=gorra)
+        if form.is_valid():
+            form.save()
+            return redirect('../../gorras')  
+    else:
+        form = GorraForm(instance=gorra)
+
+    return render(request, 'editar_gorra.html', {'form': form, 'gorra': gorra})
+
+def borrar_gorra(request, id):
+    gorra = get_object_or_404(Gorras, id=id)
+
+    if request.method == 'POST':
+        gorra.delete()
+        return redirect('../../gorras')  
+
+    return render(request, 'borrar_gorra.html', {'gorra': gorra})
+
 def post_calzado(request):
     if request.method == 'POST':
         form = CalzadoForm(request.POST)
@@ -38,6 +60,27 @@ def post_calzado(request):
     else:
         form = CalzadoForm()
     return render(request, 'calzado.html', {'form': form})
+def editar_calzado(request, id):
+    calzado = get_object_or_404(Calzado, id=id)
+
+    if request.method == 'POST':
+        form = CalzadoForm(request.POST, instance=calzado)
+        if form.is_valid():
+            form.save()
+            return redirect('../../calzado')  
+    else:
+        form = CalzadoForm(instance=calzado)
+
+    return render(request, 'editar_calzado.html', {'form': form, 'calzado': calzado})
+
+def borrar_calzado(request, id):
+    calzado = get_object_or_404(Calzado, id=id)
+
+    if request.method == 'POST':
+        calzado.delete()
+        return redirect('../../calzado')  
+
+    return render(request, 'borrar_calzado.html', {'calzado': calzado})
 
 def post_remeras(request):
     if request.method == 'POST':
@@ -48,6 +91,28 @@ def post_remeras(request):
     else:
         form = RemeraForm()
     return render(request, 'remeras.html', {'form': form})
+
+def editar_remera(request, id):
+    remera = get_object_or_404(Remeras, id=id)
+
+    if request.method == 'POST':
+        form = RemeraForm(request.POST, instance=remera)
+        if form.is_valid():
+            form.save()
+            return redirect('../../remeras')  
+    else:
+        form = RemeraForm(instance=remera)
+
+    return render(request, 'editar_remera.html', {'form': form, 'remera': remera})
+
+def borrar_remera(request, id):
+    remera = get_object_or_404(Remeras, id=id)
+
+    if request.method == 'POST':
+        remera.delete()
+        return redirect('../../remeras')  
+
+    return render(request, 'borrar_remera.html', {'remera': remera})
 
 def search_remeras(request):
     if request.method == "GET":
